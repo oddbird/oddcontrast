37db2a280ddcdb6277a75438e0cc6945bfee90e2
Ed Rivas <ed@oddbird.net>
2022-02-16 00:40:25 +0000

    Pull Request deployment demo

---
+++ src/routes/datetime.svelte
@@ -0,0 +1,3 @@
+<h1>Hello</h1>
+<p>Right now it is {new Date()}</p>
+<p><a href="/">Back</a></p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a href="/datetime">Go to the other page</a>
