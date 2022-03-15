3005280b885a2340f62bf4ecf539280e64051494
Ed Rivas <ed@oddbird.net>
2022-03-15 17:00:14 +0000

    yarn format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,8 @@
 <h1>Welcome to SvelteKit</h1>
 <p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
 <style lang="scss">
-	// Add scoped Sass here
+  // Add scoped Sass here
 </style>
