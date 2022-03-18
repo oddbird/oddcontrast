3546688aadf165db18385ce54b2fbebe01ce067b
Jonny Gerig Meyer <jonny@oddbird.net>
2022-03-18 10:26:25 -0400

    add missing file

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<!-- One way to apply global styles: https://github.com/sveltejs/kit/issues/1948#issuecomment-1011544861 -->
+<svelte:head>
+  <title>OddContrast</title>
+  <style lang="scss">
+    @use '../sass/app.scss';
+  </style>
+</svelte:head>
+
+<slot />
