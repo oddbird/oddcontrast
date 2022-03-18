a6585ffc8cef92b2392828a9e7e6e5f74c761e7a
Stacy Kvernmo <stacy@oddbird.net>
2022-03-17 23:34:35 -0500

    social nav list with a11y labels

--- src/lib/banner.svelte
+++ src/lib/banner.svelte
@@ -1,8 +1,16 @@
 <div data-layout="banner">
   <div>I am a logo</div>
-  <ul data-nav="social">
-    <li><a href="https://github.com">Social</a></li>
-    <li><a href="https://github.com">Social</a></li>
-    <li><a href="https://github.com">Social</a></li>
+  <ul data-nav="social" data-list="inline">
+    <li>
+      <a href="https://github.com">Icon <span class="sr-only">GitHub</span></a>
+    </li>
+    <li>
+      <a href="https://twitter.com">Icon <span class="sr-only">Twitter</span></a
+      >
+    </li>
+    <li>
+      <a href="https://oddbird.net">Icon <span class="sr-only">OddBird</span></a
+      >
+    </li>
   </ul>
 </div>
