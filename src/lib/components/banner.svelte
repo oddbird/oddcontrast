999dadb74938b5ab5412cabec77ed8ee8de855f2
Stacy Kvernmo <stacy@oddbird.net>
2022-03-31 10:15:59 -0500

    add links to oddbird sites for now

--- src/lib/components/banner.svelte
+++ src/lib/components/banner.svelte
@@ -8,10 +8,14 @@
   </div>
   <ul data-nav="social" data-list="inline">
     <li>
-      <a href="https://github.com">Icon<span class="sr-only">GitHub</span></a>
+      <a href="https://github.com/oddbird"
+        >Icon<span class="sr-only">GitHub</span></a
+      >
     </li>
     <li>
-      <a href="https://twitter.com">Icon<span class="sr-only">Twitter</span></a>
+      <a href="https://twitter.com/oddbird"
+        >Icon<span class="sr-only">Twitter</span></a
+      >
     </li>
     <li>
       <a href="https://oddbird.net">Icon<span class="sr-only">OddBird</span></a>
