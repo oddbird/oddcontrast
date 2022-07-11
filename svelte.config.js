9fd4499d8f26a26fb752d15a3cb69cddf77e688e
Jonny Gerig Meyer <jonny@oddbird.net>
2022-07-11 15:12:05 -0400

    Adjust to breaking svelte-kit changes

--- svelte.config.js
+++ svelte.config.js
@@ -13,40 +13,7 @@ const config = {
   }),
 
   kit: {
-    adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          $src: path.resolve('./src'),
-        },
-      },
-      test: {
-        include: ['./test/js/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-        globals: true,
-        environment: 'jsdom',
-        watch: false,
-        setupFiles: './test/js/setup.ts',
-        clearMocks: true,
-        reporters: 'dot',
-        coverage: {
-          enabled: true,
-          reporter: ['text-summary', 'html'],
-          include: ['src/**/*.{js,ts,svelte}'],
-          exclude: [
-            'src/**/*.d.ts',
-            'src/vendor',
-            'src/routes/docs.ts',
-            'src/routes/__layout.svelte',
-          ],
-          all: true,
-          skipFull: true,
-          lines: 100,
-          functions: 100,
-          branches: 50, // Seems to be a bug in Svelte component branch coverage
-          statements: 100,
-        },
-      },
-    },
+    adapter: process.env.VITEST ? undefined : adapter(),
   },
 };
 
