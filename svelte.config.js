5ffe37a1d24f516fbbbeb754cc6c84a0936186aa
Jonny Gerig Meyer <jonny@oddbird.net>
2022-12-20 20:08:31 -0500

    upgrade svelte

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
 
