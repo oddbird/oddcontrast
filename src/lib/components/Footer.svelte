888fd70068f1667793de878c1307c73173f016dd
Stacy Kvernmo <stacy@oddbird.net>
2023-03-22 12:52:43 -0500

    move socials to footer, add icons

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,10 +1,35 @@
 <script lang="ts">
+  import ExternalLink from '$lib/components/util/ExternalLink.svelte';
+  import Icon from '$lib/components/util/Icon.svelte';
   let start = 2022;
   let year = new Date().getFullYear();
+  const links = [
+    ['github', 'GitHub', 'https://github.com/oddbird/oddcontrast'],
+    ['twitter', 'Twitter', 'https://twitter.com/oddbird'],
+    ['oddbird', 'OddBird', 'https://www.oddbird.net/'],
+  ];
 </script>
 
 <footer data-layout="footer">
-  <p>
+  <span>
     &copy; {start}{year > start ? `–${year}` : ''} OddBird.
-  </p>
+  </span>
+  <ul data-nav="social" data-list="inline">
+    {#each links as [name, text, href]}
+      <li>
+        <ExternalLink {href} showIcon={false}>
+          <Icon {name} />
+          <span class="sr-only">{text}</span></ExternalLink
+        >
+      </li>
+    {/each}
+  </ul>
 </footer>
+
+<style lang="scss">
+  [data-nav='social'] {
+    --li-padding-bottom: 0;
+    --link: var(--text);
+    --link-focus: var(--action);
+  }
+</style>
--- src/lib/components/util/Icon.svelte
+++ src/lib/components/util/Icon.svelte
@@ -2,8 +2,11 @@
   import type { SvelteComponent } from 'svelte';
 
   import Check from '$lib/icons/Check.svelte';
+  import GitHub from '$lib/icons/GitHub.svelte';
   import Logo from '$lib/icons/Logo.svelte';
   import NewTab from '$lib/icons/NewTab.svelte';
+  import OddBird from '$lib/icons/OddBird.svelte';
+  import Twitter from '$lib/icons/Twitter.svelte';
   import Warning from '$lib/icons/Warning.svelte';
 
   const icons: { [key: string]: typeof SvelteComponent } = {
@@ -11,6 +14,9 @@
     logo: Logo,
     newtab: NewTab,
     warning: Warning,
+    github: GitHub,
+    twitter: Twitter,
+    oddbird: OddBird,
   };
 
   export let name: string;
