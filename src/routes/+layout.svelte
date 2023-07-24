23aa687f5d3e3638e6e88f1651c30e6b0d60e45b
Jonny Gerig Meyer <jonny@oddbird.net>
2023-07-24 14:19:31 -0400

    Add plausible analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,21 @@
 <script>
   // Import global styles
   import '$src/sass/app.scss';
+
+  // eslint-disable-next-line import/no-unresolved
+  import { CONTEXT } from '$env/static/public';
 </script>
 
 <svelte:head>
   <title>OddContrast</title>
+  <!-- Plausible analytics -->
+  {#if CONTEXT === 'production'}
+    <script
+      defer
+      data-domain="oddcontrast.com"
+      src="https://plausible.io/js/script.js"
+    ></script>
+  {/if}
 </svelte:head>
 
 <slot />
