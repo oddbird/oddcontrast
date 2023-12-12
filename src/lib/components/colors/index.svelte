0ac29162610870dbbe25aefef8681cf73cf1f572
James Stuckey Weber <jamesnw@gmail.com>
2023-12-12 16:44:51 -0500

    Remove implementation warning

--- src/lib/components/colors/index.svelte
+++ src/lib/components/colors/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Formats from '$lib/components/colors/Formats.svelte';
   import Header from '$lib/components/colors/Header.svelte';
-  import ImplementationWarning from '$lib/components/colors/ImplementationWarning.svelte';
   import Sliders from '$lib/components/colors/Sliders.svelte';
   import SupportWarning from '$lib/components/colors/SupportWarning.svelte';
   import { bg, fg, format } from '$lib/stores';
@@ -10,7 +9,6 @@
 <h2 class="sr-only">Check the contrast ratio between two colors</h2>
 
 <SupportWarning format={$format} />
-<ImplementationWarning format={$format} />
 
 <form data-form="contrast-checker" data-layout="color-form">
   <Header type="bg" color={bg} format={$format} />
