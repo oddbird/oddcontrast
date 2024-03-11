1a281649cbebe13d10cdd63ce33a134389cc4878
Jonny Gerig Meyer <jonny@oddbird.net>
2024-03-11 10:33:25 -0400

    split vite and vitest configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="vitest" />
-
 import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
 import { NodePackageImporter } from 'sass';
@@ -13,31 +11,6 @@ export default defineConfig({
         includePaths: [path.resolve('./src/sass/')],
         pkgImporter: new NodePackageImporter('.'),
       },
-    },
-  },
-  /**
-   * @see https://vitest.dev/config/#configuration
-   */
-  test: {
-    include: ['./test/js/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globals: true,
-    environment: 'jsdom',
-    watch: false,
-    setupFiles: './test/js/setup.js',
-    clearMocks: true,
-    reporters: 'dot',
-    coverage: {
-      enabled: true,
-      reporter: ['text-summary', 'html'],
-      include: ['src/**/*.{js,ts,svelte}'],
-      exclude: [
-        'src/**/*.d.ts',
-        'src/routes/styleguide/**/*',
-        'src/routes/+layout.*',
-      ],
-      all: true,
-      skipFull: true,
-      reportOnFailure: true,
     },
   },
 });
---
+++ vitest.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, mergeConfig } from 'vitest/config';
+
+import viteConfig from './vite.config.js';
+
+/**
+ * @see https://vitest.dev/config/#configuration
+ */
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      include: ['./test/js/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      globals: true,
+      environment: 'jsdom',
+      watch: false,
+      setupFiles: './test/js/setup.js',
+      clearMocks: true,
+      reporters: 'dot',
+      coverage: {
+        enabled: true,
+        reporter: ['text-summary', 'html'],
+        include: ['src/**/*.{js,ts,svelte}'],
+        exclude: [
+          'src/**/*.d.ts',
+          'src/routes/styleguide/**/*',
+          'src/routes/+layout.*',
+        ],
+        all: true,
+        skipFull: true,
+        reportOnFailure: true,
+      },
+    },
+  }),
+);
