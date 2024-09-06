1d469cc36d82f97263d1cf95c351f95d33c0c447
Jonny Gerig Meyer <jonny@oddbird.net>
2024-09-06 15:38:48 -0400

    Upgrade deps

--- test/js/@types/vitest.d.ts
+++ test/js/@types/vitest.d.ts
@@ -2,7 +2,7 @@ import { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers';
 
 declare global {
   namespace jest {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unused-vars, @typescript-eslint/ban-types
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-unused-vars
     interface Matchers<R = void, T = {}>
       extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
   }
